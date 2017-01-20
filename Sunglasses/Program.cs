4a1ed5da3948f8709a89af3d58e6bc567c74d768
Warglaive <warglaivegm@gmail.com>
2017-01-20 17:36:41 +0200

    SunglassesAdded

---
+++ Sunglasses/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sunglasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            //UpperLine
+            for (int row = 1; row <= n * 2; row++)
+            {
+                Console.Write("*");
+            }
+            for (int row = 0; row < n; row++)
+            {
+                Console.Write(" ");
+            }
+            for (int row = 1; row <= n * 2; row++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            //twoMiddleLines
+            for (int i = 0; i < n-2; i++)
+            {
+                Console.Write("*");
+                for (int row = 0; row < 2*n - 2; row++)
+                {
+                    Console.Write("/");
+                }
+                Console.Write("*");
+                if (i == (n - 1) / 2 - 1)
+                {
+                    for (int row = 0; row < n; row++)
+                    {
+                        Console.Write("|");
+                    }
+                }
+                else
+                {
+                    for (int row = 0; row < n; row++)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.Write("*");
+                for (int row = 0; row < 2 * n - 2; row++)
+                {
+                    Console.Write("/");
+                }
+                Console.Write("*");
+                Console.WriteLine();
+            }
+            //BotLines
+            for (int row = 1; row <= n * 2; row++)
+            {
+                Console.Write("*");
+            }
+            for (int row = 0; row < n; row++)
+            {
+                Console.Write(" ");
+            }
+            for (int row = 1; row <= n * 2; row++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
